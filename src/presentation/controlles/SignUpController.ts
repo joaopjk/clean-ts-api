8a20cf9cb50a80f9efe3b837d922668585c33a9a
joaopjk <joaopjk48@gmail.com>
2022-06-28 20:57:36 -0300

    refactor: move duplicated code to a helper

--- src/presentation/controlles/SignUpController.ts
+++ src/presentation/controlles/SignUpController.ts
@@ -4,12 +4,12 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-
     const requiredFields = ['name', 'email']
     for (const field of requiredFields) {
       if (!httpRequest.body[field])
         return BadRequest(new MissingParamError(field))
     }
+
     return {
       statusCode: 200,
       body: {}
