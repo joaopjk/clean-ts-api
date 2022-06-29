0c60e11b1be023d9fbe20ee6fa7af4a0129d1cf9
joaopjk <joaopjk48@gmail.com>
2022-06-28 21:38:23 -0300

    refactor: all tests refactors

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,15 +1,26 @@
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { BadRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field])
         return BadRequest(new MissingParamError(field))
     }
+
+    if (!this.emailValidator.isValid(httpRequest.body.email))
+      return BadRequest(new InvalidParamError('email'))
 
     return {
       statusCode: 200,
