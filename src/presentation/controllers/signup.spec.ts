b0c43e1f6f1aabdd4353b32ebc21e5b925c8a70c
joaopjk <joaopjk48@gmail.com>
2022-06-29 21:23:10 -0300

    test: should call EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -26,6 +26,7 @@ const makeSut = (): SutTypes => {
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provide', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -40,6 +41,7 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no name is provide', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -54,6 +56,7 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no password is provide', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -68,6 +71,7 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no passwordConfirmation is provide', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -83,7 +87,7 @@ describe('SignUp Controller', () => {
   test('Should return 400 if an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -96,4 +100,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
 })
