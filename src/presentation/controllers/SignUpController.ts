47c730a686bb75fd59714a0526766551f54089a3
joaopjk <joaopjk48@gmail.com>
2022-06-29 21:39:31 -0300

    refactor: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
-import { BadRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
@@ -18,21 +17,18 @@ export class SignUpController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field])
-          return BadRequest(new MissingParamError(field))
+          return badRequest(new MissingParamError(field))
       }
 
       if (!this.emailValidator.isValid(httpRequest.body.email))
-        return BadRequest(new InvalidParamError('email'))
+        return badRequest(new InvalidParamError('email'))
 
       return {
         statusCode: 200,
         body: {}
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
